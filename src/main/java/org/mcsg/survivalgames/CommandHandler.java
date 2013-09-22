07c31f27d71b35bac00a09da3b86cf79220d1b27
SamOatesGames <sam@samoatesgames.com>
2013-09-22 15:48:35 +0100

    Removed test command, as the source didn't exist.

--- src/main/java/org/mcsg/survivalgames/CommandHandler.java
+++ src/main/java/org/mcsg/survivalgames/CommandHandler.java
@@ -32,10 +32,7 @@ import org.mcsg.survivalgames.commands.SetSpawn;
 import org.mcsg.survivalgames.commands.Spectate;
 import org.mcsg.survivalgames.commands.SubCommand;
 import org.mcsg.survivalgames.commands.Teleport;
-import org.mcsg.survivalgames.commands.Test;
 import org.mcsg.survivalgames.commands.Vote;
-
-
 
 public class CommandHandler implements CommandExecutor {
 	private Plugin plugin;
@@ -72,9 +69,6 @@ public class CommandHandler implements CommandExecutor {
 		commands.put("list", new ListPlayers());
 		commands.put("tp", new Teleport());
 		commands.put("reload", new Reload());
-		commands.put("test", new Test());
-
-		// commands.put("sponsor", new Sponsor());
 	}
 
 	private void loadHelpInfo() {
